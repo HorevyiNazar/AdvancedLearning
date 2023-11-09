a2305b932e745716facb8418b66cf906496a213c
HorevyiNazar <goreviynazar@gmail.com>
2023-11-09 20:30:37 +0100

    1

--- AdvancedLearning/ViewModifierBootCamp.swift
+++ AdvancedLearning/ViewModifierBootCamp.swift
@@ -7,9 +7,40 @@
 
 import SwiftUI
 
+struct defaultButtonViewModifier: ViewModifier {
+    
+    let bcColor: Color
+    
+    func body(content: Content) -> some View {
+        content
+            .font(.headline)
+            .foregroundStyle(Color.white)
+            .frame(height: 55)
+            .frame(maxWidth: .infinity)
+            .background(bcColor)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+            .shadow(radius: 10)
+            .padding()
+    }
+}
+
+extension View {
+    func withDefaultButtonFormatting(bcColor: Color = .blue) -> some View {
+        modifier(defaultButtonViewModifier(bcColor: bcColor))
+    }
+}
+
 struct ViewModifierBootCamp: View {
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Hello world")
+                .withDefaultButtonFormatting(bcColor: .orange)
+            Text("Hello everyone")
+                .withDefaultButtonFormatting()
+            Text("Hello")
+                .withDefaultButtonFormatting(bcColor: .yellow)
+        }
     }
 }
 
