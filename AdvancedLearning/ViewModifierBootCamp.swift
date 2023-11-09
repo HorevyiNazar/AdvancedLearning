c7d93c8d2603568e52ef0f836651a6e89f4c69cc
HorevyiNazar <goreviynazar@gmail.com>
2023-11-09 20:22:00 +0100

    ViewModifiers

---
+++ AdvancedLearning/ViewModifierBootCamp.swift
@@ -0,0 +1,18 @@
+//
+//  ViewModifierBootCamp.swift
+//  AdvancedLearning
+//
+//  Created by Назар Горевой on 09.11.2023.
+//
+
+import SwiftUI
+
+struct ViewModifierBootCamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ViewModifierBootCamp()
+}
