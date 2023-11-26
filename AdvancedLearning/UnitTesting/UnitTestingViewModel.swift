987d96033244f72e5b9d255537e340af67133158
HorevyiNazar <goreviynazar@gmail.com>
2023-11-26 17:32:40 +0100

    Unit Test, Part 2, 59:02

--- AdvancedLearning/UnitTesting/UnitTestingViewModel.swift
+++ AdvancedLearning/UnitTesting/UnitTestingViewModel.swift
@@ -12,12 +12,39 @@ import SwiftUI
 class UnitTestingViewModel {
     var isPremium: Bool
     var dataArray: [String] = []
+    var selectedItem: String? = nil
     
     init(isPremium: Bool) {
         self.isPremium = isPremium
     }
     
     func addItem(item: String) {
+        guard !item.isEmpty else { return }
         self.dataArray.append(item)
+    }
+    func selectItem(item: String) {
+        if let x = dataArray.first(where: { $0 == item }) {
+            selectedItem = x
+        } else {
+            selectedItem = nil
+        }
+    }
+    func saveItem(item: String) throws {
+        
+        guard !item.isEmpty else {
+            throw DataError.noData
+        }
+        
+        if let x = dataArray.first(where: { $0 == item }) {
+            print("Save item here\(x)")
+        } else {
+            throw DataError.itemNotFound
+        }
+        
+    }
+    
+    enum DataError: LocalizedError {
+        case noData
+        case itemNotFound
     }
 }
--- AdvancedLearning_Tests/UnitTestingBootCampViewModel_Tests.swift
+++ AdvancedLearning_Tests/UnitTestingBootCampViewModel_Tests.swift
@@ -70,13 +70,87 @@ final class UnitTestingBootCampViewModel_Tests: XCTestCase {
         // Given
         let vm = UnitTestingViewModel(isPremium: Bool.random())
         // When
-        vm.addItem(item: "hello")
+        let loopCount: Int = Int.random(in: 1..<100)
+        for _ in 0..<loopCount {
+            vm.addItem(item: UUID().uuidString)
+        }
         // Then
         XCTAssertTrue(!vm.dataArray.isEmpty)
         XCTAssertFalse(vm.dataArray.isEmpty)
-        XCTAssertEqual(vm.dataArray.count, 1)
+        XCTAssertEqual(vm.dataArray.count, loopCount)
         XCTAssertNotEqual(vm.dataArray.count, 0)
         XCTAssertGreaterThan(vm.dataArray.count, 0)
+    }
+    func test_UnitTestingViewModel_dataArray_shouldNoAddBlankString() {
+        // Given
+        let vm = UnitTestingViewModel(isPremium: Bool.random())
+        // When
+        vm.addItem(item: "")
+        // Then
+        XCTAssertTrue(vm.dataArray.isEmpty)
+    }
+    func test_UnitTestingViewModel_selectedItem_shouldStartNil() {
+        // Given
+        let vm = UnitTestingViewModel(isPremium: Bool.random())
+        // When
+        
+        // Then
+        XCTAssertTrue(vm.selectedItem == nil)
+        XCTAssertNil(vm.selectedItem)
+    }
+    func test_UnitTestingViewModel_selectedItem_shouldBeNilWhenSelectingInvalidItem() {
+        // Given
+        let vm = UnitTestingViewModel(isPremium: Bool.random())
+        // When
+        // Select valid item
+        let newItem = UUID().uuidString
+        vm.addItem(item: newItem)
+        vm.selectItem(item: newItem)
+        // Select invalid item
+        vm.selectItem(item: UUID().uuidString)
+        // Then
+        XCTAssertNil(vm.selectedItem)
+    }
+    func test_UnitTestingViewModel_selectedItem_shouldBeSelected() {
+        // Given
+        let vm = UnitTestingViewModel(isPremium: Bool.random())
+        // When
+        let newItem = UUID().uuidString
+        vm.addItem(item: newItem)
+        vm.selectItem(item: newItem)
+        // Then
+        XCTAssertNotNil(vm.selectedItem)
+        XCTAssertEqual(vm.selectedItem, newItem)
+    }
+    func test_UnitTestingViewModel_selectedItem_shouldBeSelected_stress() {
+        // Given
+        let vm = UnitTestingViewModel(isPremium: Bool.random())
+        // When
+        let loopCount: Int = Int.random(in: 0..<100)
+        var itemsArray: [String] = []
+        
+        for _ in 0..<loopCount {
+            let newItem = UUID().uuidString
+            vm.addItem(item: newItem)
+            itemsArray.append(newItem)
+        }
+        let randomItem = itemsArray.randomElement() ?? ""
+        vm.selectItem(item: randomItem)
+        // Then
+        XCTAssertNotNil(vm.selectedItem)
+        XCTAssertEqual(vm.selectedItem, randomItem)
+    }
+    func test_UnitTestingViewModel_saveItem_shouldThrowError_itemNotFound() {
+        // Given
+        let vm = UnitTestingViewModel(isPremium: Bool.random())
+        // When
+        
+        // Then
+        XCTAssertThrowsError(try vm.saveItem(item: UUID().uuidString))
+        XCTAssertThrowsError(try vm.saveItem(item: UUID().uuidString), "Should throw Item Not Found Error") { error in
+            let returnedError = error as? UnitTestingViewModel.DataError
+            XCTAssertEqual(returnedError, UnitTestingViewModel.DataError.itemNotFound)
+        }
     }
 }
 
